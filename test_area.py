6e15ece911005777046ae01a6a68373fdb34921a
shreyamchakrasali-hash <shreyamchakrasali@gmail.com>
2026-01-19 12:41:58 +0530

    Update test_area.py

--- test_area.py
+++ test_area.py
@@ -1,3 +1,3 @@
 from area import area_cicumference
 def test_area_cicumference():
-    assert area_cicumference(3) == "Area: 28.274333882308138, Circumference: 18.84955592153876"
+    assert area_cicumference(5) == "Area: 28.274333882308138, Circumference: 18.84955592153876"

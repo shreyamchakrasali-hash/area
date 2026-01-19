b1998e87b1bb94f51bf997a3c1374386fe3fdfa2
shreyamchakrasali-hash <shreyamchakrasali@gmail.com>
2026-01-19 12:51:29 +0530

    Update test_area.py

--- test_area.py
+++ test_area.py
@@ -1,4 +1,6 @@
+from area import area_circumference
+
 def test_area_circumference():
-    assert area_circumference(5) == (
-        "Area:78.53981633974483, Circumference:31.41592653589793"
-    )
+    result = area_circumference(5)
+    expected = "Area:78.53981633974483, Circumference:31.41592653589793"
+    assert result == expected

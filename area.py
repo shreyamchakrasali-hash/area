f4fc83caecfbbb3b011f2cf459347d68908719c5
vaishnavi-honnimani <01fe24bca154@kletech.ac.in>
2026-01-19 12:12:13 +0530

    save both the file

---
+++ area.py
@@ -0,0 +1,11 @@
+import math
+def area_cicumference(radius):
+    area=math.pi*radius*radius
+    circumference=2*math.pi*radius
+    result=(
+        f"Area: {area}",
+        f"Circumference: {circumference}"
+    )
+
+if __name__ == "__main__":
+    print(area_cicumference(3))
---
+++ test_area.py
@@ -0,0 +1,3 @@
+from area import area_cicumference
+def test_areacase():
+    assert area_cicumference(3) == "Area: 28.274333882308138, Circumference: 18.84955592153876"

e1e293e3c14c978ca3bef884ca6728efc39e78f9
shreyamchakrasali-hash <shreyamchakrasali@gmail.com>
2026-01-19 12:46:40 +0530

    Refactor area calculation function and improve precision
    Refactored area_circumference function to area_cicumference and updated calculations to use math.pi.

--- area.py
+++ area.py
@@ -1,14 +1,9 @@
-def area_circumference(radius):
-    area = 3.14 * radius * radius
-    circumference = 2 * 3.14 * radius
-    return (
-        f"Area : {area}",
-        f"Circumference : {circumference}"
-    )
+import math
+
+def area_cicumference(radius):
+    area = math.pi * radius * radius
+    circumference = 2 * math.pi * radius
+    return f"Area:{area}, Circumference:{circumference}"
 
 if __name__ == "__main__":
-    radius = 5
-    result = area_circumference(radius)
-
-    for value in result:
-        print(value)
+    print(area_cicumference(5))

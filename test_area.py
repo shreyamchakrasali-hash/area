801e9af1ec64882c2727bfb89805e29a1826e1e6
shreyamchakrasali-hash <shreyamchakrasali@gmail.com>
2026-01-19 12:26:01 +0530

    Update test_area.py

--- test_area.py
+++ test_area.py
@@ -1,3 +1,6 @@
 from area import area_cicumference
+import math
+
 def test_areacase():
-    assert area_cicumference(3) == "Area: 28.274333882308138, Circumference: 18.84955592153876"
+    assert area_cicumference(5) == f"Radius : 5\nArea : {math.pi*25}\nCircumference : {2*math.pi*5}"
+    
